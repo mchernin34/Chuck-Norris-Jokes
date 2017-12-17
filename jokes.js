34cc07715a5e08284c40b4e4ed1c01253571307d
mchernin34 <mchernin21@gmail.com>
2017-12-17 23:30:04 +0200

    Added axios

--- jokes.js
+++ jokes.js
@@ -1,11 +1,11 @@
+import axios from 'axios'
+
 export const jokes = {
   getOne: function() {
     return new Promise((resolve, reject) => {
-      fetch('http://api.icndb.com/jokes/random')
-        .then(res => res.json())
-        .then(data => {
-          resolve(data.value.joke)
-        })
+      axios.get('http://api.icndb.com/jokes/random').then(res => {
+        resolve(res.data.value.joke)
+      })
     })
   }
 }
